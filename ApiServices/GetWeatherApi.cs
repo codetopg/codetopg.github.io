8b40050ccc9ebc689e72f297923ae8d5995149bb
Tom Dinh <tomdinhnz@outlook.com>
2022-10-25 01:22:14 +0700

    add openweather api

---
+++ ApiServices/GetWeatherApi.cs
@@ -0,0 +1,40 @@
+﻿using codeTopGBlazorWasm.Models;
+using Microsoft.JSInterop;
+using System.Text.Json;
+
+namespace codeTopGBlazorWasm.ApiServices
+{
+    public class GetWeatherApi
+    {
+        private Coord? position;
+        private OpenWeatherModel weatherData;
+        private string? _url;
+        private HttpClient _httpClient;
+       
+
+        public GetWeatherApi(HttpClient httpClient)
+        {
+            _httpClient = httpClient ?? throw new AggregateException(nameof(httpClient));
+            
+        }
+        
+        public async Task GetForcast()
+        {
+            string APIKey = "1f7e64a683b4c7085e693af48155414a";
+
+
+            _url = $"api.openweathermap.org/data/2.5/forecast?" +
+                $"lat={position.lat}" +
+                $"&lon={position.lon}" +
+                $"&appid={APIKey}";
+
+            Console.WriteLine(_url);
+            var httpResponseMessage = await _httpClient.GetAsync(_url);
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            var stream = await httpResponseMessage.Content.ReadAsStringAsync();
+            weatherData = JsonSerializer.Deserialize<OpenWeatherModel>(stream);
+
+        }       
+    }
+}
---
+++ wwwroot/scripts/getLocation.js
@@ -0,0 +1,16 @@
+﻿var getLocationInterop = {};
+
+getLocationInterop.getLocation = async function () {
+    function getPositionAsync() {
+        return new Promise((success, error) =>
+        {navigator.geolocation.getCurrentPosition(success, error) });
+    }
+    if (navigator.geolocation) {
+        var position = await getPositionAsync();
+        var coords = {
+            longitude: position.coords.longitude,
+            latitude: position.coords.latitude
+        }
+        return coords
+    } else { throw Error="Browser not support Geo location!" }
+}
