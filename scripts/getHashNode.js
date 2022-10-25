d7b7d0c651c9b64889dd8f3ad4058a5a1e5c6325
codetopg <112187544+codetopg@users.noreply.github.com>
2022-10-25 17:36:48 +0000

    Deploying to gh-pages from @ codetopg/codetopg.github.io@cc10305e4a534d68fdea6c2cc44e52805edc8859 🚀

---
+++ scripts/getHashNode.js
@@ -0,0 +1,29 @@
+﻿getHashNodeInterop = {};
+
+const query = `
+{
+  user(username: "codetopg") {
+    publication {
+      posts(page: 0) {
+       slug
+       title
+       brief
+       coverImage
+      }
+    }
+  }
+}`;
+
+getHashNodeInterop.getData = async function gql() {
+    const response = await fetch("https://api.hashnode.com", {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ query })
+    });
+    const body = await response.json();
+    console.log(body);
+    return body;
+    
+}
