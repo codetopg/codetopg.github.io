cc10305e4a534d68fdea6c2cc44e52805edc8859
Tom Dinh <tomdinhnz@outlook.com>
2022-10-26 00:35:44 +0700

    add get blog api from hashnode

---
+++ Models/HashNodeModel.cs
@@ -0,0 +1,32 @@
+﻿namespace codeTopGBlazorWasm.Models
+{
+    public class HashNodeModel
+    {
+        public Data data { get; set; }
+    }
+
+    public class Data
+    {
+        public User user { get; set; }
+    }
+
+    public class User
+    {
+        public Publication publication { get; set; }
+    }
+
+    public class Publication
+    {
+        public Post[] posts { get; set; }
+    }
+
+    public class Post
+    {
+        public string title { get; set; }
+        public string brief { get; set; }
+        public string slug { get; set; }
+        public string coverImage { get; set; }
+
+    }
+
+}
