cc9b6aee0b925138546252ff340eb701e0396160
Tom Dinh <tomdinhnz@outlook.com>
2022-11-01 22:23:45 +0700

    add scoll to top for blog modal

--- ApiServices/GetWeatherApi.cs
+++ ApiServices/GetWeatherApi.cs
@@ -4,23 +4,24 @@ using System.Text.Json;
 
 namespace codeTopGBlazorWasm.ApiServices
 {
-    public class GetWeatherApi : IGetWeatherApi
+    public class GetWeatherApi : IGetWeatherApi, IDisposable
     {
-        public CoordsModel location;       
+        public CoordsModel location;
         private OpenWeatherModel weatherData;
-        private HttpClient _httpClient;
-
-        StringBuilder myUrl = new();
+        private readonly HttpClient _httpClient;
+        readonly StringBuilder myUrl = new();
 
         public GetWeatherApi(HttpClient httpClient)
         {
             _httpClient = httpClient ?? throw new AggregateException(nameof(httpClient));
         }
 
+
+
         public async Task<OpenWeatherModel> GetForcast(double lon, double lat)
         {
             //HttpClientConfig();
-            Console.WriteLine("LOCATION " + lat + lon);
+            //Console.WriteLine("LOCATION " + lat + lon);
             myUrl.Append("https://api.openweathermap.org/data/2.5/forecast?");
             myUrl.Append($"lat={lat}");
             myUrl.Append($"&lon={lon}");
@@ -34,29 +35,30 @@ namespace codeTopGBlazorWasm.ApiServices
 
             using var stream = await httpResponseMessage.Content.ReadAsStreamAsync();
             weatherData = await JsonSerializer.DeserializeAsync<OpenWeatherModel>(stream);
-            Console.WriteLine("weatherData from API service "+ weatherData.city.name);
+            Console.WriteLine("weatherData from API service " + weatherData?.city.name);
             return weatherData;
         }
 
-        private async void HttpClientConfig()
-        {
-            //location = await coords.GetCoord();
-            //var lat;
-            //IgetCoords.GetCoord().Result.Latitude lat = new();
-            //IgetCoords.GetCoord().Result.Longitude = new();
+        //private void HttpClientConfig()
+        //{
+        //    location = await coords.GetCoord();
+        //    var lat;
+        //    IgetCoords.GetCoord().Result.Latitude lat = new();
+        //    IgetCoords.GetCoord().Result.Longitude = new();
 
-           
 
-            //Console.WriteLine("LOCATION" + lat + lon);
-            //myUrl.Append("https://api.openweathermap.org/data/2.5/forecast?");
-            //myUrl.Append($"lat={lat}");
-            //myUrl.Append($"&lon={lon}");
-            //myUrl.Append("&appid=1f7e64a683b4c7085e693af48155414a");
 
-            
-            //Console.WriteLine("_url in HttpClient", _url);
-            //_httpClient.BaseAddress = new Uri(_url);
-           
-        }
+        //    Console.WriteLine("LOCATION" + lat + lon);
+        //    myUrl.Append("https://api.openweathermap.org/data/2.5/forecast?");
+        //    myUrl.Append($"lat={lat}");
+        //    myUrl.Append($"&lon={lon}");
+        //    myUrl.Append("&appid=1f7e64a683b4c7085e693af48155414a");
+
+
+        //    Console.WriteLine("_url in HttpClient", _url);
+        //    _httpClient.BaseAddress = new Uri(_url);
+
+        //}
+        public void Dispose() => _httpClient.Dispose();
     }
 }
--- ApiServices/HashNodeGqlApi.cs
+++ ApiServices/HashNodeGqlApi.cs
@@ -12,17 +12,18 @@ namespace codeTopGBlazorWasm.ApiServices
             this.js = js;
         }
 
-
-
         public async Task<HashNodeModel> GetAllBlogPosts()
         {
-            string query = "{user(username: \"codetopg\") {publication {posts(page: 0) {slug title brief coverImage dateUpdated }}}}";
+            string query = "{user(username: \"codetopg\")" +
+                " {publication {posts(page: 0) " +
+                "{slug title brief coverImage dateUpdated }}}}";
             return await js.InvokeAsync<HashNodeModel>("getHashNodeInterop.getData", query);
         }
 
         public async Task<HashNodePostModel> GetPost(string slug)
         {
-            string query = $"{{post(slug:\"{slug}\", hostname:\"\") {{ content title }} }}";
+            string query = $"{{post(slug:\"{slug}\", hostname:\"\")" +
+                $" {{ content title }} }}";
             return await js.InvokeAsync<HashNodePostModel>("getHashNodeInterop.getData",query);
         }
 
